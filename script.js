5633f426a9ce2ea0e3b248bed1da7d5f168d6d96
Shaggy Tech Guy <18csmith2@Carters-MacBook-Air.local>
2018-03-31 20:00:06 -0400

    Still Missing Bright Party
    Added all parties and assets EXCEPT for the Bright Party whos website is not yet turned in and a picture of the Assistant (Leadership Team). Code still needs some cleaning or whatever, but it's functional. Also fixed video file sizes to be under GitHub's 100 MB limit.

--- script.js
+++ script.js
@@ -19,6 +19,57 @@ function toVice() {
 	pic.src = "Images/Golden/alexander.png";
 }
 
+function switchCandidate(party, toCandidate) {
+	// Not actually all Golden Party, just named that because Golden was the only Party to utilize this at first
+	
+	var title = document.getElementById("GoldenTitle")
+	var subtitle = document.getElementById("GoldenSubtitle")
+	var p1 = document.getElementById("GoldenP1")
+	var p2 = document.getElementById("GoldenP2")
+	
+	var pres = document.getElementById("GoldenPresident")
+	var vp = document.getElementById("GoldenVice")
+	var pic = document.getElementById("GoldenCandidate")
+	
+	if (party == "Golden") {
+		if (toCandidate == "Alexander") {
+			title.innerHTML = "MEET ALEXANDER J. FORRY"
+			subtitle.innerHTML = "Activist, Lobbyist (2002 - )"
+			p1.innerHTML = "Alexander J. Forry was born in York, Pennsylvania. He was selected by Mr. Trevor Blood to be his Vice-President presiding over the Golden Party in early March 2017 to begin the rise of a “Golden Age.” In early years, he began his education at Central York School District, where he was very successful in investing his time in various clubs and activities. He was  and is a very involved person within his community with the Boy Scouts of America and National Honor Society. He was the captain of the boys swim team furthering showcasing his leadership skills. For his undergraduate degree, he attended American University to study Politics, Policy, and Law, where he met his running mate for President, Trevor Blood. "
+			p2.innerHTML = "They founded the Golden Party with the help of Mr. Brent Forry, and Mr. Clayton Brosend. He then went on to pursue his Law degree from Columbia and became a member of the New York BAR Association. His love for traveling took him all over the world during his time spent in school having studied abroad in over 6 countries including Spain, France, and Greenland. After graduation, he worked at a few firms before deciding to pursue work in activist campaigns. He has been involved in many different movements such as Women’s March, the Not One More campaign, and many more. From there he was able to move into starting his own lobbying firm to be able to push for policy change to help support the causes he is an activist for. His motivation is that of the Golden Party, to equally represent everyone, despite the background of any person."
+	
+			pres.style.textDecoration = "none";
+			vp.style.textDecoration = "underline";
+			pic.src = "Images/Golden/alexander.png";
+		} else {
+			title.innerHTML = "MEET TREVOR V. BLOOD"
+			subtitle.innerHTML = "Activist, Government Official (2000 - )"
+			p1.innerHTML = "Trevor V. Blood was born in York, Pennsylvania. He elected to run the Golden Party in early March 2017 to begin the rise of a “Golden Age.” In early years, he began his education at Central York School District, where he invested many hours and time into various clubs and organizations. He was and is a very involved person within his community with National Honor Society and the International Thespian Society. He worked as a Guest Service Ambassador at Target of East York. In his college years, he attended American University to study Politics and United States Government, where he met his running mate for Vice-President, Alexander Forry."
+			p2.innerHTML = "They founded the Golden Party with the help of Mr. Brent Forry, and Mr. Clayton Brosend. After graduating from American University, he attended New York University for his doctorate, further pursuing his love for politics. He even minored again in French language to be an ambassador to France. In later year, he grew more passionate in politics, and he began protesting social issues that inhibits people the rights that they equally deserved. He attended marches for women and was an activist in the Not One More and the Me Too movements. He works for equality for all, as the whole basis of the Golden Party is to equally represent everyone, despite the background of any person."
+	
+			pres.style.textDecoration = "underline";
+			vp.style.textDecoration = "none";
+			pic.src = "Images/Golden/trevor.png";
+		}
+	} else {
+		if (toCandidate == "Delaney") {
+			title.innerHTML = "Delaney Boylan"
+			p1.innerHTML = "Delaney Boylan is a forty two year old woman and a member of the Libere party. She is married to Bryce Boylan and has two kids: Rebecca, 15, and Thomas, 12. Mrs. Boylan grew up in Mississippi, but moved to Pennsylvania after college. She attended The University of Mississippi and majored in political science with a minor in business. Once she acquired her degree, she worked her way up through competition and ran for the House of Representatives in Pennsylvania. She has served six years as a representative of Pennsylvania. Recently, she was asked to be the Vice Presidential candidate for Michael Gilliland. Boylan plans to run alongside him and give her party the platform it deserves."
+			
+			pres.style.textDecoration = "none";
+			vp.style.textDecoration = "underline";
+			pic.src = "Images/Libere/delaney.jpg";
+		} else {
+			title.innerHTML = "Michael Gilliland"
+			p1.innerHTML = "Mr. Michael Gilliland is the President under the political party, Libere. He is fifty four years old and has a wife, Ellen Gilliland, along with three children: Amber, Angie and Aaron. Mr. Gilliland grew up in York, Pennsylvania with his mother and father. After attending Central York High School, he continued his education at Harvard University where he received his masters degree in finance. After attaining his degree, he moved to New York and acquired a position as a statistician. Mr. Gilliland decided he wanted to move back to Pennsylvania where he could pursue a political career. Michael became mayor of York and fulfilled his eight years with successful track records and production. He restored many buildings and created many Homeless shelters in York City. Now, Michael is running for United States President under the political party, Libere."
+			
+			pres.style.textDecoration = "underline";
+			vp.style.textDecoration = "none";
+			pic.src = "Images/Libere/michael.jpg";
+		}
+	}
+}
+
 function toPresident() {
 	var title = document.getElementById("GoldenTitle")
 	var subtitle = document.getElementById("GoldenSubtitle")
